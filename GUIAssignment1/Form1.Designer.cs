57982fc9aa6ea78ed7a86494d1806dfe06a7d82c
Maros Gorny <maros.gorny@gmail.com>
2023-10-29 00:09:20 +0200

    add lat and long

---
+++ GUIAssignment1/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace GUIAssignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox2 = new GroupBox();
+            radioButton1 = new RadioButton();
+            radioButton3 = new RadioButton();
+            numericUpDown3 = new NumericUpDown();
+            numericUpDown4 = new NumericUpDown();
+            label1 = new Label();
+            label4 = new Label();
+            groupBox1 = new GroupBox();
+            radioButton5 = new RadioButton();
+            radioButton2 = new RadioButton();
+            numericUpDown1 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).BeginInit();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(radioButton1);
+            groupBox2.Controls.Add(radioButton3);
+            groupBox2.Controls.Add(numericUpDown3);
+            groupBox2.Controls.Add(numericUpDown4);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Location = new Point(259, 205);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(282, 68);
+            groupBox2.TabIndex = 33;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Longitude";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton1.Location = new Point(6, 23);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(55, 39);
+            radioButton1.TabIndex = 21;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "N";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton3.Location = new Point(65, 23);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(49, 39);
+            radioButton3.TabIndex = 20;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "S";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Location = new Point(118, 31);
+            numericUpDown3.Maximum = new decimal(new int[] { 90, 0, 0, 0 });
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(55, 27);
+            numericUpDown3.TabIndex = 14;
+            // 
+            // numericUpDown4
+            // 
+            numericUpDown4.Location = new Point(194, 31);
+            numericUpDown4.Maximum = new decimal(new int[] { 90, 0, 0, 0 });
+            numericUpDown4.Name = "numericUpDown4";
+            numericUpDown4.Size = new Size(55, 27);
+            numericUpDown4.TabIndex = 15;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(175, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(20, 30);
+            label1.TabIndex = 17;
+            label1.Text = "°";
+            // 
+            // label4
+            // 
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(255, 28);
+            label4.Name = "label4";
+            label4.Size = new Size(16, 30);
+            label4.TabIndex = 18;
+            label4.Text = "'";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton5);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(numericUpDown1);
+            groupBox1.Controls.Add(numericUpDown2);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Location = new Point(259, 131);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(282, 68);
+            groupBox1.TabIndex = 32;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Latitude";
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton5.Location = new Point(6, 23);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(55, 39);
+            radioButton5.TabIndex = 21;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "N";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton2.Location = new Point(65, 23);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(49, 39);
+            radioButton2.TabIndex = 20;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "S";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(118, 31);
+            numericUpDown1.Maximum = new decimal(new int[] { 90, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(55, 27);
+            numericUpDown1.TabIndex = 14;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(194, 31);
+            numericUpDown2.Maximum = new decimal(new int[] { 90, 0, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(55, 27);
+            numericUpDown2.TabIndex = 15;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(175, 28);
+            label2.Name = "label2";
+            label2.Size = new Size(20, 30);
+            label2.TabIndex = 17;
+            label2.Text = "°";
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(255, 28);
+            label3.Name = "label3";
+            label3.Size = new Size(20, 30);
+            label3.TabIndex = 18;
+            label3.Text = "'";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(259, 291);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 31;
+            button1.Text = "Search";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton3;
+        private NumericUpDown numericUpDown3;
+        private NumericUpDown numericUpDown4;
+        private Label label1;
+        private Label label4;
+        private GroupBox groupBox1;
+        private RadioButton radioButton5;
+        private RadioButton radioButton2;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+    }
+}
