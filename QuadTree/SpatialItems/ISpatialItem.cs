4edcd8af94f6c154ac410ce612b019a5ee397e43
Maros Gorny <maros.gorny@gmail.com>
2023-12-16 15:11:16 +0100

    rework parcel and property

--- QuadTree/SpatialItems/ISpatialItem.cs
+++ QuadTree/SpatialItems/ISpatialItem.cs
@@ -127,8 +127,8 @@ public class Point : ISpatialItem
 /// </summary>
 public class Rectangle : ISpatialItem
 {
-    public Point LowerLeft { get; private set; }
-    public Point UpperRight { get; private set; }
+    public Point LowerLeft { get; set; }
+    public Point UpperRight { get; set; }
 
     public Rectangle(Point bottomLeft, Point topRight)
     {
@@ -230,6 +230,33 @@ public class GPSPoint : Point
         Y = longVal * (double)longDir;
     }
 
+    public void Serialize(BinaryWriter writer)
+    {
+        writer.Write((int)LatitudeDirection);
+        writer.Write((int)LongitudeDirection);
+        writer.Write(X);
+        writer.Write(Y);
+    }
+
+    public static GPSPoint Deserialize(BinaryReader reader)
+    {
+        var latDir = (LatitudeDirection)reader.ReadInt32();
+        var longDir = (LongitudeDirection)reader.ReadInt32();
+        var x = reader.ReadDouble();
+        var y = reader.ReadDouble();
+
+        if (latDir == LatitudeDirection.S)
+        {
+            x *= -1;
+        }
+
+        if (longDir == LongitudeDirection.W)
+        {
+            y *= -1;
+        }
+        return new GPSPoint(latDir, x, longDir, y);
+    }
+
     public override string ToString()
     {
         string xString = X.ToString("F13").TrimEnd('0');
@@ -240,6 +267,23 @@ public class GPSPoint : Point
 
         return $"{LatitudeDirection} {xString}, {LongitudeDirection} {yString}";
     }
+
+    public int GetSize()
+    {
+        int size = 0;
+
+        size += sizeof(int); // Size for LatitudeDirection
+        size += sizeof(int); // Size for LongitudeDirection
+        size += sizeof(double); // Size for X
+        size += sizeof(double); // Size for Y
+
+        size += sizeof(int); // Size for LatitudeDirection
+        size += sizeof(int); // Size for LongitudeDirection
+        size += sizeof(double); // Size for X
+        size += sizeof(double); // Size for Y
+
+        return size;
+    }
 }
 
 public class GPSRectangle : Rectangle
@@ -249,5 +293,4 @@ public class GPSRectangle : Rectangle
         : base(bottomLeft, topRight)
     {
     }
-
 }
--- SemesterAssignment2/Program.cs
+++ SemesterAssignment2/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace SemesterAssignment2
+﻿using QuadTreeDS.SpatialItems;
+using SemesterAssignment2.RealtyObjects;
+using System.Collections;
+
+
+namespace SemesterAssignment2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a GPSRectangle for the test
+            GPSPoint lowerLeft = new GPSPoint(LatitudeDirection.N, 40.7128, LongitudeDirection.W, 74.0060);
+            GPSPoint upperRight = new GPSPoint(LatitudeDirection.N, 40.9138, LongitudeDirection.W, 73.8060);
+            GPSRectangle gpsRectangle = new GPSRectangle(lowerLeft, upperRight);
+
+            // Create a Parcel object for testing
+            Parcel parcel = new Parcel(1, "Test Parcel", gpsRectangle);
+            parcel.TryAddProperty(101);
+            parcel.TryAddProperty(102);
+
+            // Serialize the Parcel object
+            byte[] serializedParcel = parcel.ToByteArray();
+
+            // Deserialize to get a new Parcel object
+            Parcel deserializedParcel = new Parcel(0, "", new GPSRectangle(new GPSPoint(LatitudeDirection.N, 0, LongitudeDirection.E, 0), new GPSPoint(LatitudeDirection.N, 0, LongitudeDirection.E, 0)));
+            deserializedParcel = deserializedParcel.FromByteArray(serializedParcel);
+
+            // Test equality and hash code
+            bool areEqual = parcel.MyEquals(deserializedParcel);
+            bool parcelsEqual = parcel.MyEquals(deserializedParcel);
+
+            // Output the results
+            Console.WriteLine("Are the original and deserialized Parcel equal? " + areEqual);
+            Console.WriteLine("Original Parcel Size: " + parcel.GetSize());
+            Console.WriteLine("Deserialized Parcel Size: " + deserializedParcel.GetSize());
         }
     }
 }
